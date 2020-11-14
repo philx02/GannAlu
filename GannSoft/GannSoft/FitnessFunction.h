bfffedd33d12f7eb042cf3b04c7ba967eb857866
Philippe Cayouette <26016182+philx02@users.noreply.github.com>
2020-11-14 15:30:13 -0500

    no idea

--- GannSoft/GannSoft/FitnessFunction.h
+++ GannSoft/GannSoft/FitnessFunction.h
@@ -1,16 +1,168 @@
 #pragma once
 
+#include <cmath>
+#include <boost/math/constants/constants.hpp>
+#include <boost/optional.hpp>
+#include <algorithm>
+
+#undef min
+
+namespace bmc = boost::math::double_constants;
+
+static const double sLocalGravitationalAcceleration = 9.81;
+
+class LongitudinalAircraft
+{
+public:
+  LongitudinalAircraft(double iRotationalInertia, double iElevatorToCogDistance, double iMass, double iElevatorCommandToForceOnTailFactor, double iThrottleCommandToThrustFactor, double iLiftConstant, double iDragConstant, double iPitchAngle, double iPitchAngularSpeed, double iVerticalPosition, double iVerticalSpeed, double iHorizontalPosition, double iHorizontalSpeed)
+    : mRotationalInertia(iRotationalInertia)
+    , mElevatorToCogDistance(iElevatorToCogDistance)
+    , mMass(iMass)
+    , mElevatorCommandToForceOnTailFactor(iElevatorCommandToForceOnTailFactor)
+    , mThrottleCommandToThrustFactor(iThrottleCommandToThrustFactor)
+    , mLiftConstant(iLiftConstant)
+    , mDragConstant(iDragConstant)
+  {
+    initialize(iPitchAngle, iPitchAngularSpeed, iVerticalPosition, iVerticalSpeed, iHorizontalPosition, iHorizontalSpeed);
+  }
+
+  inline void initialize(double iPitchAngle, double iPitchAngularSpeed, double iVerticalPosition, double iVerticalSpeed, double iHorizontalPosition, double iHorizontalSpeed)
+  {
+    mPitchAngle = iPitchAngle;
+    mPitchAngularSpeed = iPitchAngularSpeed;
+    mVerticalPosition = iVerticalPosition;
+    mVerticalSpeed = iVerticalSpeed;
+    mHorizontalPosition = iHorizontalPosition;
+    mHorizontalSpeed = iHorizontalSpeed;
+  }
+
+  inline double pitchAngle() const
+  {
+    return mPitchAngle;
+  }
+  inline double pitchAngularSpeed() const
+  {
+    return mPitchAngularSpeed;
+  }
+
+  inline double verticalPosition() const
+  {
+    return mVerticalPosition;
+  }
+  inline double verticalSpeed() const
+  {
+    return mVerticalSpeed;
+  }
+
+  inline double horizontalPosition() const
+  {
+    return mHorizontalPosition;
+  }
+  inline double horizontalSpeed() const
+  {
+    return mHorizontalSpeed;
+  }
+
+  inline double bodySpeed() const
+  {
+    return std::sqrt(mHorizontalSpeed * mHorizontalSpeed + mVerticalSpeed * mVerticalSpeed);
+  }
+
+  // iElevatorCommand => [-1, 1]; iThrottleCommand => [0, 1]
+  inline void perform(double iElevatorCommand, double iThrottleCommand, double iDeltaT)
+  {
+    auto wForceOnTail = iElevatorCommand * mElevatorCommandToForceOnTailFactor;
+    auto wThrust = iThrottleCommand * mThrottleCommandToThrustFactor;
+
+    // Rotational dynamics
+    auto wTorque = wForceOnTail * mElevatorToCogDistance;
+    auto wPitchAngularAcceleration = wTorque / mRotationalInertia;
+    mPitchAngularSpeed += wPitchAngularAcceleration * iDeltaT;
+    mPitchAngle += mPitchAngularSpeed * iDeltaT;
+
+    // Translational dynamics
+    auto wBodySpeed = bodySpeed();
+    // this is an approximation
+    auto wLift = mLiftConstant * wBodySpeed;
+    auto wDrag = mDragConstant * wBodySpeed;
+
+    auto wTheta = mPitchAngle + bmc::half_pi;
+    auto wVerticalLiftComponent = wLift * std::sin(wTheta);
+    auto wHorizontalLiftComponent = wLift * std::cos(wTheta);
+    auto wPsi = mPitchAngle + bmc::pi;
+    auto wVerticalDragComponent = wDrag * std::sin(wPsi);
+    auto wHorizontalDragComponent = wDrag * std::cos(wPsi);
+    auto wVerticalThrustComponent = wThrust * std::sin(mPitchAngle);
+    auto wHorizontalThrustComponent = wThrust * std::cos(mPitchAngle);
+
+    auto wTotalVerticalForce = wVerticalLiftComponent + wVerticalDragComponent + wVerticalThrustComponent - mMass * sLocalGravitationalAcceleration;
+    auto wTotalHorizontalForce = wHorizontalLiftComponent + wHorizontalDragComponent + wHorizontalThrustComponent;
+
+    auto wVerticalAcceleration = wTotalVerticalForce / mMass;
+    mVerticalSpeed += wVerticalAcceleration * iDeltaT;
+    mVerticalPosition += mVerticalSpeed * iDeltaT;
+
+    auto wHorizontalAcceleration = wTotalHorizontalForce / mMass;
+    mHorizontalSpeed += wHorizontalAcceleration * iDeltaT;
+    mHorizontalPosition += mHorizontalSpeed * iDeltaT;
+  }
+
+private:
+  double mPitchAngle;
+  double mPitchAngularSpeed;
+
+  double mVerticalPosition;
+  double mVerticalSpeed;
+
+  double mHorizontalPosition;
+  double mHorizontalSpeed;
+
+  // aircraft performance data
+  double mRotationalInertia;
+  double mElevatorToCogDistance;
+  double mMass;
+  double mElevatorCommandToForceOnTailFactor;
+  double mThrottleCommandToThrustFactor;
+  double mLiftConstant;
+  double mDragConstant;
+};
+
+static const double sRotationalInertia = 0.1;
+static const double sElevatorToCogDistance = 1;
+static const double sMass = 1;
+static const double sElevatorCommandToForceOnTailFactor = 1;
+static const double sThrottleCommandToThrustFactor = 10;
+static const double sLiftConstant = 1.22625;
+static const double sDragConstant = 1.22625/7;
+
+static const auto sEquilibriumHorizontalSpeed = (sMass * sLocalGravitationalAcceleration) / sLiftConstant;
+
 // This function must be thread safe
-template< typename Ann >
-double fitnessFunction(const Ann &iAnn)
+template< typename Ann, typename EndOfIterationFunction >
+double fitnessFunction(const Ann &iAnn, EndOfIterationFunction &iEndOfIterationFunction)
 {
   Ann::Input wInput;
   double wScore = 0;
-  for (double wIter = 0; wIter < 6.28; wIter += 0.1)
+  double wTargetAltitude = 100;
+  double wTargetSpeed = 8;
+  LongitudinalAircraft wLongitudinalAircraft(sRotationalInertia, sElevatorToCogDistance, sMass, sElevatorCommandToForceOnTailFactor, sThrottleCommandToThrustFactor, sLiftConstant, sDragConstant, 0, 0, 50, 0, 0, sEquilibriumHorizontalSpeed);
+  double wDeltaT = 0.1;
+  for (double wTime = 0; wTime < 30; wTime += wDeltaT)
   {
-    wInput[0] = wIter;
+    wInput[0] = wLongitudinalAircraft.pitchAngle();
+    wInput[1] = wLongitudinalAircraft.pitchAngularSpeed();
+    wInput[2] = wLongitudinalAircraft.verticalPosition();
+    wInput[3] = wLongitudinalAircraft.verticalSpeed();
+    wInput[4] = wLongitudinalAircraft.horizontalPosition();
+    wInput[5] = wLongitudinalAircraft.horizontalSpeed();
+
     auto wOutput = iAnn.compute(wInput);
-    wScore -= std::abs(wOutput[0] - wIter);
+    wLongitudinalAircraft.perform(wOutput[0]*2-1, wOutput[1], wDeltaT);
+    wScore -= std::abs(wLongitudinalAircraft.verticalPosition() - wTargetAltitude);
+    wScore -= std::abs(wLongitudinalAircraft.bodySpeed() - wTargetSpeed) * (50/8);
+    auto wPitchError = std::abs(wLongitudinalAircraft.pitchAngle());
+    wScore -= std::min(0.25*std::exp(7.0*wPitchError), 200.0);
+    iEndOfIterationFunction(wTime, wOutput, wLongitudinalAircraft);
   }
   return wScore;
 }
--- GannSoft/GannSoft/main.cpp
+++ GannSoft/GannSoft/main.cpp
@@ -12,14 +12,27 @@
 #include <fstream>
 #include <iomanip>
 
-inline double logisticFunction(const double &iInput)
+template< typename T, std::size_t Span, std::size_t Offset >
+inline T logisticFunction(const T &iInput)
 {
-  return 2.0 / (1.0 + std::exp(-iInput)) - 1.0;
+  static const auto wSpan = static_cast< T >(Span);
+  static const auto wOffset = static_cast< T >(Offset);
+  static const auto wOne = static_cast< T >(1);
+  return wSpan / (wOne + std::exp(-iInput)) + wOffset;
 };
 
-inline double linearFunction(const double &iInput)
+template< typename T >
+inline double linearFunction(const T &iInput)
 {
   return iInput;
+}
+
+template< typename T, std::size_t LowerLimit, std::size_t HigherLimit >
+inline double saturatedLinearFunction(const T &iInput)
+{
+  static const auto wLowerLimit = static_cast< T >(LowerLimit);
+  static const auto wHigherLimit = static_cast< T >(HigherLimit);
+  return iInput > wHigherLimit ? wHigherLimit : (iInput < wLowerLimit ? wLowerLimit : iInput);
 }
 
 auto wInitialWeightsDistribution = std::uniform_real_distribution<>(-1.0, 1.0);
@@ -28,7 +41,7 @@ std::ofstream wOut;
 
 auto wCreateAnn = [&]()
 {
-  return createAndInitializeArtificialNeuralNetwork< double, sInputLayerSize, sHiddenLayerSize, sOutputLayerSize >(logisticFunction, linearFunction, [&]() -> double {return wInitialWeightsDistribution(wMt19937); });
+  return createAndInitializeArtificialNeuralNetwork< double, sInputLayerSize, sHiddenLayerSize, sOutputLayerSize >(logisticFunction< double, 1, 0 >, logisticFunction< double, 1, 0 >, [&]() -> double {return wInitialWeightsDistribution(wMt19937); });
 };
 
 typedef decltype(wCreateAnn()) Ann;
@@ -64,8 +77,37 @@ std::ostream & operator<<(std::ostream &iOstream, const Ann &iAnn)
   return iOstream;
 }
 
+void caracterize()
+{
+  LongitudinalAircraft wLongitudinalAircraft(sRotationalInertia, sElevatorToCogDistance, sMass, sElevatorCommandToForceOnTailFactor, sThrottleCommandToThrustFactor, sLiftConstant, sDragConstant, 0, 0, 50, 0, 0, sEquilibriumHorizontalSpeed);
+
+  double wDeltaT = 0.1;
+  std::ofstream wFile("test.csv");
+  wFile << "Time,Elevator,Throttle,Altitude,Speed,pitch\n";
+  double KPpitch = 0.1;
+  double KDpitch = -0.5;
+  double KPthrottle = 5;
+  for (double wTime = 0; wTime < 30; wTime += wDeltaT)
+  {
+    double wDesiredPitch = (100 - wLongitudinalAircraft.verticalPosition()) / 50 * bmc::pi/4;
+    double wPitchError = wDesiredPitch - wLongitudinalAircraft.pitchAngle();
+    double wElevator = KPpitch * wPitchError + KDpitch * wLongitudinalAircraft.pitchAngularSpeed();
+    wElevator = wElevator > 1 ? 1 : wElevator < -1 ? -1 : wElevator;
+    
+    double wDesiredSpeed = 8;
+    double wSpeedError = wDesiredSpeed - wLongitudinalAircraft.bodySpeed();
+    double wThrottle = KPthrottle * wSpeedError;
+    wThrottle = wThrottle > 1 ? 1 : wThrottle < 0 ? 0 : wThrottle;
+    
+    wLongitudinalAircraft.perform(wElevator, wThrottle, wDeltaT);
+    wFile << wTime << "," << wElevator << "," << wThrottle << "," << wLongitudinalAircraft.verticalPosition() << "," << wLongitudinalAircraft.bodySpeed() << "," << wLongitudinalAircraft.pitchAngle() << "\n";
+  }
+  exit(0);
+}
+
 int main()
 {
+  //caracterize();
   auto wGa = createGeneticAlgorithm< double, Ann >(1000, wCreateAnn);
 
   typedef decltype(wGa.begin()) RatedIndividualIterator;
@@ -90,28 +132,32 @@ int main()
     return true;
   };
 
-  wGa.runGenerations(10000, fitnessFunction< Ann >, wReproductionFunction, [](RatedIndividualIterator iBegin, RatedIndividualIterator iEnd) {}, wPostGenerationEvaluation);
-  auto &&wBestAnn = wGa.begin()->mIndividual;
-
+  std::ofstream wFile;
+  auto wOutput = [&](double iTime, const Ann::Output &iOutput, const LongitudinalAircraft &iLongitudinalAircraft)
   {
-    std::ofstream wCsv("best.csv");
-    std::array< double, 1 > wInput;
-    for (double wIter = 0; wIter < 6.28; wIter += 0.1)
-    {
-      wInput[0] = wIter;
-      wCsv << wIter << "," << wBestAnn.compute(wInput)[0] << "\n";
-    }
-  }
+    double wAltitudeContribution = std::abs(iLongitudinalAircraft.verticalPosition() - 100);
+    double wSpeedContribution = std::abs(iLongitudinalAircraft.bodySpeed() - 8) * (50 / 8);
+    double wPitchError = std::abs(iLongitudinalAircraft.pitchAngle());
+    double wPitchContribution = std::min(0.25*std::exp(7.0*wPitchError), 200.0);
+    wFile << iTime << "," << (iOutput[0] * 2 - 1) << "," << iOutput[1] << "," << iLongitudinalAircraft.verticalPosition() << "," << iLongitudinalAircraft.bodySpeed() << "," << iLongitudinalAircraft.pitchAngle() << "," << wAltitudeContribution << "," << wSpeedContribution << "," << wPitchContribution << "\n";
+  };
 
+  wFile.open("Performance/random.csv");
+  fitnessFunction(wCreateAnn(), wOutput);
+  wFile.close();
+
+  while (true)
   {
-    auto wRandomAnn = wCreateAnn();
-    std::ofstream wCsv("random.csv");
-    std::array< double, 1 > wInput;
-    for (double wIter = 0; wIter < 6.28; wIter += 0.1)
-    {
-      wInput[0] = wIter;
-      wCsv << wIter << "," << wRandomAnn.compute(wInput)[0] << "\n";
-    }
+    auto wNoOp = [](double, const Ann::Output &, const LongitudinalAircraft &) {};
+    wGa.runGenerations(1000, [&](const Ann &iIndividual) {return fitnessFunction(iIndividual, wNoOp); }, wReproductionFunction, [](RatedIndividualIterator iBegin, RatedIndividualIterator iEnd) {}, wPostGenerationEvaluation);
+    auto &&wBestAnn = wGa.begin()->mIndividual;
+
+    std::ostringstream wFilename;
+    wFilename << "Performance/best_of_gen" << std::setw(10) << std::setfill('0') << boost::lexical_cast< std::string >(wGeneration) << ".csv";
+    wFile.open(wFilename.str().c_str());
+    wFile << "Time,ElevatorCommand,ThrottleCommand,Altitude,Speed,Pitch\n";
+    fitnessFunction(wBestAnn, wOutput);
+    wFile.close();
   }
 
   return 0;
