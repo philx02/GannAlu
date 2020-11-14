d8fc70824050b3fed4d817b29326606ac371632c
Philippe Cayouette <pcayouette@spoluck.ca>
2020-11-14 15:30:13 -0500

    no idea

--- GannSoft/GannSoft/Definitions.h
+++ GannSoft/GannSoft/Definitions.h
@@ -4,14 +4,9 @@
 
 #include <cstddef>
 
-static const std::size_t sInputLayerSize = 1;
-static const std::size_t sHiddenLayerSize = 3;
-static const std::size_t sOutputLayerSize = 1;
-
-static const std::size_t sInputToHiddenWeightSize = sInputLayerSize*sHiddenLayerSize;
-static const std::size_t sHiddenToOutputWeightSize = sHiddenLayerSize*sOutputLayerSize;
-static const std::size_t sTotalWeightSize = sInputToHiddenWeightSize + sHiddenToOutputWeightSize;
-static const std::size_t sTotalBiasesSize = sHiddenLayerSize + sOutputLayerSize;
+static const std::size_t sInputLayerSize = 6;
+static const std::size_t sHiddenLayerSize = 8;
+static const std::size_t sOutputLayerSize = 2;
 
 static const std::size_t sTotalNumberOfWeightMutations = boost::static_unsigned_max< sTotalWeightSize / 20, 1 >::value;
 static const std::size_t sTotalNumberOfBiasesMutations = boost::static_unsigned_max< sTotalBiasesSize / 20, 1 >::value;
